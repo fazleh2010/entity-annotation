15afbc7336d20cbe8f0a287d9fafe8424c3ee3e4
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2020-12-14 18:37:10 +0100

    reactor

--- src/main/resources/politicians.cmd
+++
@@ -1,9 +0,0 @@
-cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Person>" | sed --expression 's/ <http:\/\/www.w3.*//' > persons.txt
-
-
-
-cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Flim>" | sed --expression 's/ <http:\/\/www.w3.*//' > flims.txt
-
-
-cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Mountain>" | sed --expression 's/ <http:\/\/www.w3.*//' > Mountain.txt
-
